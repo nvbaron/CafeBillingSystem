5bba76fc5f5c7cb470be508873f209806e581d23
nvbaron <ReyNRud@reynruds-mbp.home>
2016-12-02 12:27:54 +0000

    Cafe billing system Test cases added

--- src/test/scala/service/BillingServiceSpec.scala
+++ src/test/scala/service/BillingServiceSpec.scala
@@ -12,4 +12,19 @@ class BillingServiceSpec extends FlatSpec with Matchers {
     val itemList = Nil
     assertResult(df.format(BillingService.calculateBill(itemList)))("0.00")
   }
+  
+  it should " give sum of items price as 2 pounds 50 pence " in {
+		val itemList = "Cola" :: "Cheese Sandwich" :: Nil
+		assertResult(df.format(BillingService.calculateBill(itemList)))("2.50")
+	}
+  
+  it should " give sum of items price as 3 pounds 50 pence " in {
+		val itemList = "Cola" :: "Coffee" :: "Cheese Sandwich" :: Nil
+		assertResult(df.format(BillingService.calculateBill(itemList)))("3.50")
+	}
+  
+  it should " give sum of items price as 8 pounds 0 pence " in {
+		val itemList = "Cola" :: "Coffee" :: "Cheese Sandwich" :: "Steak Sandwich" :: Nil
+		assertResult(df.format(BillingService.calculateBill(itemList)))("8.00")
+	}
 }
